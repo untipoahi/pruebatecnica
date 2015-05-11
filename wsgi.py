eac4eabf6e05749955c6cf3f937cca7c3adc5f0f
BuildTools <unconfigured@null.spigotmc.org>
2015-05-11 18:49:18 -0430

    Wohooonano .openshift/action_hooks/deploy

--- wsgi.py
+++ wsgi.py
@@ -6,13 +6,13 @@ from pyramid.paster import get_app
 
 #app = config.make_wsgi_app()
 path = '/'.join(os.path.abspath(__file__).split('/')[0:-1])
-app = get_app(path + '/pyramidapp/development.ini', 'main')
+application = get_app(path + '/pyramidapp/development.ini', 'main')
 
 #
 # Below for testing only
 #
 if __name__ == '__main__':
 	from wsgiref.simple_server import make_server
-	httpd = make_server('0.0.0.0', 3000, app)
+	httpd = make_server('0.0.0.0', 6543, application)
 	
 	httpd.serve_forever()
