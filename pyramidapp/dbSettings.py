ffb35abb9eadc66b4b06cde1d3abbaf5c142bdc2
BuildTools <unconfigured@null.spigotmc.org>
2015-05-18 18:39:54 -0430

    fixed db settings and app port

--- pyramidapp/dbSettings.py
+++ pyramidapp/dbSettings.py
@@ -4,7 +4,7 @@ DATABASE = {
     'drivername': 'postgres',
     'host': os.getenv('OPENSHIFT_POSTGRESQL_DB_HOST','localhost'),
     'port': os.getenv('OPENSHIFT_POSTGRESQL_DB_PORT','5432'),
-    #'username': 'adminq3lqhtw',
-    #'password': 'RBUtalL4hRxQ',
+    'username': 'adminq3lqhtw',
+    'password': 'RBUtalL4hRxQ',
     'database': 'pyramidapp'
 }
